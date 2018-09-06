88d98db55f4233063718af8f5c51d8e25e567801
Muhammad Ghalib <36465177+gabe108@users.noreply.github.com>
2018-09-06 16:10:05 +1000

    Camera zoom and move feature working

--- Assets/Scripts/Camera/CameraControl.cs
+++ Assets/Scripts/Camera/CameraControl.cs
@@ -2,103 +2,102 @@
 
 public class CameraControl : MonoBehaviour
 {
-    public float m_DampTime = 0.2f;                 
-    public float m_ScreenEdgeBuffer = 4f;           
-    public float m_MinSize = 6.5f;                  
-    [HideInInspector] public Transform[] m_Targets; 
+	public float m_DampTime = 0.2f;                 // Approximate time for the camera to refocus.
+	public float m_ScreenEdgeBuffer = 4f;           // Space between the top/bottom most target and the screen edge.
+	public float m_MinSize = 6.5f;                  // The smallest orthographic size the camera can be.
+	public Transform[] m_Targets;					// All the targets the camera needs to encompass.
 
 
-    private Camera m_Camera;                        
-    private float m_ZoomSpeed;                      
-    private Vector3 m_MoveVelocity;                 
-    private Vector3 m_DesiredPosition;              
+	private Camera m_Camera = null;                        // Used for referencing the camera.
+	private float m_ZoomSpeed;                      // Reference speed for the smooth damping of the orthographic size.
+	private Vector3 m_MoveVelocity;                 // Reference velocity for the smooth damping of the position.
+	private Vector3 m_DesiredPosition;              // The position the camera is moving towards.
 
-
-    private void Awake()
+	private void Awake()
     {
-        m_Camera = GetComponentInChildren<Camera>();
+		m_Camera = GetComponentInChildren<Camera>();
     }
 
 
     private void FixedUpdate()
     {
-        Move();
-        Zoom();
+		Move();
+		Zoom();
     }
 
 
     private void Move()
     {
-        FindAveragePosition();
+		FindAveragePosition();
 
-        transform.position = Vector3.SmoothDamp(transform.position, m_DesiredPosition, ref m_MoveVelocity, m_DampTime);
+		transform.position = Vector3.SmoothDamp(
+			transform.position, 
+			m_DesiredPosition,
+			ref m_MoveVelocity, 
+			m_DampTime);
     }
 
 
     private void FindAveragePosition()
     {
-        Vector3 averagePos = new Vector3();
-        int numTargets = 0;
+		Vector3 averagePos = new Vector3();
+		int numTargets = 0;
 
-        for (int i = 0; i < m_Targets.Length; i++)
-        {
-            if (!m_Targets[i].gameObject.activeSelf)
-                continue;
+		for(int i = 0; i < m_Targets.Length; i++)
+		{
+			if (!m_Targets[i].gameObject.activeSelf)
+				continue;
 
-            averagePos += m_Targets[i].position;
-            numTargets++;
-        }
+			averagePos += m_Targets[i].position;
+			numTargets++;
+		}
 
-        if (numTargets > 0)
-            averagePos /= numTargets;
+		if (numTargets > 0)
+			averagePos /= numTargets;
 
-        averagePos.y = transform.position.y;
+		averagePos.y = transform.position.y;
 
-        m_DesiredPosition = averagePos;
+		m_DesiredPosition = averagePos;
     }
 
 
     private void Zoom()
     {
-        float requiredSize = FindRequiredSize();
-        m_Camera.orthographicSize = Mathf.SmoothDamp(m_Camera.orthographicSize, requiredSize, ref m_ZoomSpeed, m_DampTime);
-    }
+		float requiredSize = FindRequiredSize();
+		m_Camera.orthographicSize = Mathf.SmoothDamp(m_Camera.orthographicSize, requiredSize, ref m_ZoomSpeed, m_DampTime);
+	}
 
 
     private float FindRequiredSize()
     {
-        Vector3 desiredLocalPos = transform.InverseTransformPoint(m_DesiredPosition);
+		Vector3 desiredLocalPos = transform.InverseTransformPoint(m_DesiredPosition);
+		float size = 0.0f;
 
-        float size = 0f;
+		for (int i = 0; i < m_Targets.Length; i++)
+		{
+			if (!m_Targets[i].gameObject.activeSelf)
+				continue;
 
-        for (int i = 0; i < m_Targets.Length; i++)
-        {
-            if (!m_Targets[i].gameObject.activeSelf)
-                continue;
+			Vector3 targetLocalPos = transform.InverseTransformPoint(m_Targets[i].position);
+			Vector3 desiredPosToTarget = targetLocalPos - desiredLocalPos;
+			size = Mathf.Max(size, Mathf.Abs(desiredLocalPos.y));
+			size = Mathf.Max(size, Mathf.Abs(desiredPosToTarget.x) / m_Camera.aspect);
 
-            Vector3 targetLocalPos = transform.InverseTransformPoint(m_Targets[i].position);
+		}
 
-            Vector3 desiredPosToTarget = targetLocalPos - desiredLocalPos;
+		size += m_ScreenEdgeBuffer;
+		size = Mathf.Max(size, m_MinSize);
 
-            size = Mathf.Max (size, Mathf.Abs (desiredPosToTarget.y));
-
-            size = Mathf.Max (size, Mathf.Abs (desiredPosToTarget.x) / m_Camera.aspect);
-        }
-        
-        size += m_ScreenEdgeBuffer;
-
-        size = Mathf.Max(size, m_MinSize);
-
-        return size;
+		return size;
     }
 
 
     public void SetStartPositionAndSize()
     {
-        FindAveragePosition();
+		FindAveragePosition();
 
-        transform.position = m_DesiredPosition;
+		transform.position = m_DesiredPosition;
 
-        m_Camera.orthographicSize = FindRequiredSize();
-    }
+		m_Camera.orthographicSize = FindRequiredSize();
+	}
 }
