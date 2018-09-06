c96d3b348a7948b164405b0aade78bdb68c692c7
Muhammad Ghalib <36465177+gabe108@users.noreply.github.com>
2018-09-06 16:39:28 +1000

    TankManager added [NOT TESTED}

--- Assets/Scripts/Managers/TankManager.cs
+++ Assets/Scripts/Managers/TankManager.cs
@@ -19,48 +19,48 @@ public class TankManager
 
     public void Setup()
     {
-        m_Movement = m_Instance.GetComponent<TankMovement>();
-        m_Shooting = m_Instance.GetComponent<TankShooting>();
-        m_CanvasGameObject = m_Instance.GetComponentInChildren<Canvas>().gameObject;
+		m_Movement = m_Instance.GetComponent<TankMovement>();
+		m_Shooting = m_Instance.GetComponentInChildren<TankShooting>();
+		m_CanvasGameObject = m_Instance.GetComponentInChildren<Canvas>().gameObject;
 
-        m_Movement.m_PlayerNumber = m_PlayerNumber;
-        m_Shooting.m_PlayerNumber = m_PlayerNumber;
+		m_Movement.m_PlayerNumber = m_PlayerNumber;
+		m_Shooting.m_PlayerNumber = m_PlayerNumber;
 
-        m_ColoredPlayerText = "<color=#" + ColorUtility.ToHtmlStringRGB(m_PlayerColor) + ">PLAYER " + m_PlayerNumber + "</color>";
+		m_ColoredPlayerText = "<color=#" + ColorUtility.ToHtmlStringRGB(m_PlayerColor) + ">PLAYER " + m_PlayerNumber + "</color>";
 
-        MeshRenderer[] renderers = m_Instance.GetComponentsInChildren<MeshRenderer>();
+		MeshRenderer[] renderers = m_Instance.GetComponentsInChildren<MeshRenderer>();
 
-        for (int i = 0; i < renderers.Length; i++)
-        {
-            renderers[i].material.color = m_PlayerColor;
-        }
-    }
+		for(int i = 0; i < renderers.Length; i++)
+		{
+			renderers[i].material.color = m_PlayerColor;
+		}
+	}
 
 
     public void DisableControl()
     {
-        m_Movement.enabled = false;
-        m_Shooting.enabled = false;
+		m_Movement.enabled = false;
+		m_Movement.enabled = false;
 
-        m_CanvasGameObject.SetActive(false);
+		m_CanvasGameObject.SetActive(false);
     }
 
 
     public void EnableControl()
     {
-        m_Movement.enabled = true;
-        m_Shooting.enabled = true;
+		m_Movement.enabled = true;
+		m_Movement.enabled = true;
 
-        m_CanvasGameObject.SetActive(true);
-    }
+		m_CanvasGameObject.SetActive(true);
+	}
 
 
     public void Reset()
     {
-        m_Instance.transform.position = m_SpawnPoint.position;
-        m_Instance.transform.rotation = m_SpawnPoint.rotation;
+		m_Instance.transform.position = m_SpawnPoint.position;
+		m_Instance.transform.rotation = m_SpawnPoint.rotation;
 
-        m_Instance.SetActive(false);
-        m_Instance.SetActive(true);
-    }
+		m_Instance.SetActive(false);
+		m_Instance.SetActive(true);
+	}
 }
